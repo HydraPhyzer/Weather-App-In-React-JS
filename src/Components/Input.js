852631930f6d51e7d960b8cc5b9c2fefe9e28f6b
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-07 08:00:48 +0500

    Initial Commit

---
+++ src/Components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Input.css'
+
+const Input = () => {
+    return (
+        <div className='Inp'>
+            <input type="text" placeholder='Enter Any City'/>
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default Input
---
+++ src/Components/TempDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TempDisplay.css'
+
+const TempDisplay = () => {
+    return (
+        <div className="A1-Display">
+            <p>
+                25&deg; C
+            </p>
+            <span>DELHI/INDIA</span>
+        </div>
+    )
+}
+export default TempDisplay
